3f1e88f65918c3e09de378379f66e5e0c25bfc72
Tanimismat <tanim.ismat@gmail.com>
2022-05-23 09:34:19 +0600

    banner

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -6,11 +6,14 @@ const Blogs = () => {
             <div>
                 <h6>How will you improve the performance of a react application?</h6>
                 <li>Using the production build </li>
-                <p></p>
                 <p>Profiling the components with the devtools</p>
                 <p>Virtualizing long lists using react window</p>
                 <p>Using fragment to avoid adding extra nodes to DOM</p>
                 <p>Optimizing app loading time with code splitting</p>
+            </div>
+            <div>
+                <h5>What are the different ways to manage a state in a react application?</h5>
+
             </div>
         </div>
     );
