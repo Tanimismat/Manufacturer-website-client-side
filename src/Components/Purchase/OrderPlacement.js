1d4a8a4ab8f42b16a50da09efaf6beb34dec1839
Tanimismat <tanim.ismat@gmail.com>
2022-06-14 22:44:19 +0600

    place order section

--- src/Components/Purchase/OrderPlacement.js
+++ src/Components/Purchase/OrderPlacement.js
@@ -4,7 +4,9 @@ const OrderPlacement = () => {
     return (
         <div>
             <h4>place order</h4>
-            <textarea className='form-control' id="exampleFormControlTextarea1" name="" cols="30" rows="10"></textarea>
+            <textarea className='form-control' id="exampleFormControlTextarea1" name="" cols="30" rows="10" placeholder='Address'></textarea>
+            <input type="tel" name="phone" id="phone" placeholder='Phone Number'/>
+            <input type="submit" value="Complete Purchase" />
         </div>
     );
 };
