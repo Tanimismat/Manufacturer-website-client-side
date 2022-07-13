d58d55c9318be57e0db49f51d48122b44ccc9001
Tanim Ismat <tanim.ismat@gmail.com>
2022-07-13 11:44:02 +0600

    Business summary

--- src/Components/HomePage/BusinessSummary.js
+++ src/Components/HomePage/BusinessSummary.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import * as FiIcons from "react-icons/fi";
+import * as BsIcons from "react-icons/bs";
+import * as AiIcons from "react-icons/ai";
+import * as MdIcons from "react-icons/md";
 
 const BusinessSummary = () => {
     return (
         <div>
-            <h4>MILLIONS OF BUSINESS TRUST US</h4>
-            <h6>TRY TO UNDERSTAND USERS EXPECTATION</h6>
-            <div>
-                
+            <h4 className='text-2xl text-center mt-16'>MILLIONS OF BUSINESS TRUST US</h4>
+            <h6 className='text-center mb-10'>TRY TO UNDERSTAND USERS EXPECTATION</h6>
+            <div className='flex flex-wrap'>
+                <div className='pl-36 grid justify-items-center '>
+                    <FiIcons.FiFlag size={70}/>
+                    <p className='text-center font-bold text-3xl'>70</p>
+                    <p className='text-2xl'>Countries</p>
+                </div>
+                <div className='pl-36 grid justify-items-center '>
+                    <AiIcons.AiOutlineFundProjectionScreen size={70}/>
+                    <p className='text-center font-bold text-3xl'>250+</p>
+                    <p className='text-2xl'>Happy Clients</p>
+                </div>
+                <div className='pl-36 grid justify-items-center '>
+                    <BsIcons.BsTools size={70}/>
+                    <p className='text-center font-bold text-3xl'>40+</p>
+                    <p className='text-2xl'>Tools</p>
+                </div>
+                <div className='pl-36 grid justify-items-center '>
+                    <MdIcons.MdOutlineReviews size={70}/>
+                    <p className='text-center font-bold text-3xl'>30K+</p>
+                    <p className='text-2xl'>Reviews</p>
+                </div>
             </div>
         </div>
     );
--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -5,19 +5,16 @@ import { faEnvelope } from "react-icons/fa";
 const Footer = () => {
     return (
         <div>
-            {/* <!-- Footer --> */}
             <footer className="text-center text-lg-start bg-light text-muted">
                 {/* <!-- Section: Social media --> */}
                 <section
                     className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
                 >
-                    {/* <!-- Left --> */}
+                    
                     <div className="me-5 d-none d-lg-block">
                         <span>Get connected with us on social networks:</span>
                     </div>
-                    {/* <!-- Left --> */}
-
-                    {/* <!-- Right --> */}
+                   
                     <div>
                         <a href="" className="me-4 text-reset">
                             <i className="fab fa-facebook-f"></i>
@@ -38,7 +35,7 @@ const Footer = () => {
                             <i className="fab fa-github"></i>
                         </a>
                     </div>
-                    {/* <!-- Right --> */}
+                    
                 </section>
                 {/* <!-- Section: Social media --> */}
 
@@ -58,30 +55,6 @@ const Footer = () => {
                                     dolor sit amet, consectetur adipisicing elit.
                                 </p>
                             </div>
-                            {/* <!-- Grid column --> */}
-
-                            {/* <!-- Grid column --> */}
-                            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                                {/* <!-- Links --> */}
-                                <h6 className="text-uppercase fw-bold mb-4">
-                                    Products
-                                </h6>
-                                <p>
-                                    <a href="#!" className="text-reset">Angular</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">React</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Vue</a>
-                                </p>
-                                <p>
-                                    <a href="#!" className="text-reset">Laravel</a>
-                                </p>
-                            </div>
-                            {/* <!-- Grid column --> */}
-
-                            {/* <!-- Grid column --> */}
                             <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
                                 {/* <!-- Links --> */}
                                 <h6 className="text-uppercase fw-bold mb-4">
@@ -100,9 +73,7 @@ const Footer = () => {
                                     <a href="#!" className="text-reset">Help</a>
                                 </p>
                             </div>
-                            {/* <!-- Grid column --> */}
-
-                            {/* <!-- Grid column --> */}
+                           
                             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                                 {/* <!-- Links --> */}
                                 <h6 className="text-uppercase fw-bold mb-4">
@@ -116,7 +87,7 @@ const Footer = () => {
                                 <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
                                 <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
                             </div>
-                            {/* <!-- Grid column --> */}
+                            
                         </div>
                         {/* <!-- Grid row --> */}
                     </div>
