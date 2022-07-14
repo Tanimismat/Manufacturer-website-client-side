3ee02b8e0ab4039365384275b81708f860d0e438
Tanim Ismat <tanim.ismat@gmail.com>
2022-07-14 11:15:19 +0600

    Homepage review section

--- src/Components/HomePage/Banner.js
+++ src/Components/HomePage/Banner.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import banner1 from '../../images/banner1.png';
+import React from "react";
+import banner2 from "../../images/banner-2.png";
 
 const Banner = () => {
-    return (
-        <div>
-            <img style={{ width: '100vw' }} src={banner1} alt="" />
-        </div>
-    );
+	return (
+		<div className="my-10">
+			<div class="hero bg-base-200 p-24">
+				<div class="hero-content flex-col lg:flex-row-reverse">
+					<img
+						src={banner2}
+						class="max-w-sm rounded-lg shadow-2xl"
+						alt="Album"
+					/>
+					<div>
+						<h1 class="text-5xl font-bold">Box Office News!</h1>
+						<p class="py-6">
+							Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+							excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+							et a id nisi.
+						</p>
+						<button class="btn btn-primary">Get Started</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
+
+// lg:card-side
--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -1,60 +1,76 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
+import React from "react";
+import { Link } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const Header = () => {
-  const [user] = useAuthState(auth);
-  const logout = () => {
-    signOut(auth);
-    localStorage.removeItem('accessToken');
-  };
+	const [user] = useAuthState(auth);
+	const logout = () => {
+		signOut(auth);
+		localStorage.removeItem("accessToken");
+	};
 
-  const menuItems = <>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/purchase">Purchase</Link></li>
-    <li><Link to="/blogs">Blogs</Link></li>
-    <li>
-      {
-        user? <button onClick={logout}>Log out</button> : <Link to="/login">Login</Link>
-      }
-    </li>
-    <li>{!user &&   <Link to="/register">Register</Link>}</li>
-    <li>{user && <Link to="/dashboard">Dashboard</Link>}</li>
-  </>
+	const menuItems = (
+		<>
+			<li>
+				<Link to="/">Home</Link>
+			</li>
+			<li className="pl-5">
+				<Link to="/purchase">Purchase</Link>
+			</li>
+			<li className="pl-5">
+				<Link to="/blogs">Blogs</Link>
+			</li>
+			<li className="pl-5">
+				{user ? (
+					<button onClick={logout}>Log out</button>
+				) : (
+					<Link to="/login">Login</Link>
+				)}
+			</li>
+			<li>{!user && <Link to="/register">Register</Link>}</li>
+			<li>{user && <Link to="/dashboard">Dashboard</Link>}</li>
+		</>
+	);
 
-  
-  return (
-  <>
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex="0" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </label>
-          <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            {menuItems}
-          </ul>
-        </div>
-        <a className="btn btn-ghost normal-case text-xl">Manufacturer Website</a>
-        </div>
-        
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal p-0">
-          {menuItems}
-        </ul>
-      </div> 
+	return (
+		<>
+			<div className="sticky top-0 z-50 p-6 navbar bg-base-100">
+				<div className="navbar-start">
+					<div className="dropdown">
+						<label tabIndex="0" className="btn btn-ghost lg:hidden">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-5 w-5"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor">
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M4 6h16M4 12h8m-8 6h16"
+								/>
+							</svg>
+						</label>
+						<ul
+							tabIndex="1"
+							className="menu menu-compact dropdown-content mt-3 p-5 shadow bg-base-100 rounded-box w-52">
+							{menuItems}
+						</ul>
+					</div>
+					<Link to="/" className="btn btn-ghost normal-case text-xl pl-24">
+						Manufacturer Website
+					</Link>
+				</div>
 
-        <div className="navbar-end">
-          <label tabIndex="1" htmlFor="my-drawer-2" className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </label>
-        </div>  
-
-</div> 
-        </>
-    );
+				<div className="navbar-center hidden lg:flex">
+					<ul className="menu menu-horizontal p-0">{menuItems}</ul>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Header;
