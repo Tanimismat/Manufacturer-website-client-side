f3e8e9d1675da0a5283e2946c285617b3d539228
Tanim Ismat <tanim.ismat@gmail.com>
2022-07-17 18:18:21 +0600

    README.md

--- src/Components/MyPortfolio/MyPortfolio.js
+++ src/Components/MyPortfolio/MyPortfolio.js
@@ -17,29 +17,29 @@ const MyPortfolio = () => {
 							Science & Engineering, University of Rajshahi
 						</p>
 						<p>Technologies I know as a web developer: </p>
-						<li>JS Frontend library -- React</li>
-						<li>CSS framework -- Bootstrap</li>
-						<li>Vanilla CSS</li>
+						<li className="ml-3">JS Frontend library -- React</li>
+						<li className="ml-3">CSS framework -- Bootstrap</li>
+						<li className="ml-3">Vanilla CSS</li>
 						<p>Familiar with:</p>
-						<li>JS runtime -- Node.js</li>
-						<li>Database -- MongoDB</li>
-						<li>CSS framework -- Tailwind CSS</li>
+						<li className="ml-3">JS runtime -- Node.js</li>
+						<li className="ml-3">Database -- MongoDB</li>
+						<li className="ml-3">CSS framework -- Tailwind CSS</li>
 						<p>Links of three of my projects:</p>
-						<li>
+						<li className="ml-3">
 							<a
 								className="hover:underline underline-offset-2 hover:text-primary"
 								href="https://independent-service-prov-fd90d.web.app/">
 								Your success mentor
 							</a>
 						</li>
-						<li>
+						<li className="ml-3">
 							<a
 								className="hover:underline underline-offset-2 hover:text-primary"
 								href="https://profound-genie-32ff25.netlify.app/">
 								Laptop-analysis-website
 							</a>
 						</li>
-						<li>
+						<li className="ml-3">
 							<a
 								className="hover:underline underline-offset-2 hover:text-primary"
 								href="https://hungry-babbage-6ee332.netlify.app/">
